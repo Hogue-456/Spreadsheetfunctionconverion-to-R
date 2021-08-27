c48d99133d19607f413710fc05584a4b0000c5eb
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 19:09:22 +0200

    Delete Zweitebenverarbeitung.R

--- Zweitebenverarbeitung.R
+++
@@ -1,5 +0,0 @@
-Formel <- c('=C2&"Hallo Welt"','=TEIL(C2;3;5)','=WENN(C2=3;1;0)','=LÄNGE(C2)','=TEIL(C2;3;5)','=WENN(C2=3;1;0)')
-Funktionsstartliste <- data.frame(Formel)
-Funktionsstartliste$Nummer <- 1
-Funktionsstartliste$Nummer <- cumsum(Funktionsstartliste$Nummer)
-source('~/Tabellenkalkultionsumwandler/Argumentlistenschleife.R', encoding = 'UTF-8')
