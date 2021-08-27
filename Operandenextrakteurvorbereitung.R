540edbc5092023ffaee0d50bc97ddd9533491ede
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 19:09:30 +0200

    Delete Operandenextrakteurvorbereitung.R

--- Operandenextrakteurvorbereitung.R
+++
@@ -1,4 +0,0 @@
-Relteil <- Entscheidung
-Relteil <- sqldf(" SELECT * FROM Relteil WHERE Substr IN('&','+','-','*','/') ")
-Relteil$Operatoren <- 1
-source('~/Tabellenkalkultionsumwandler/Operandenextrakteur.R', encoding = 'UTF-8')
