3320521e4f6b7ed1688296099498bba344c2d3ad
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 19:10:30 +0200

    Delete Programmoutput225.R

--- Programmoutput225.R
+++
@@ -1,9 +0,0 @@
-Entscheidungshilfe225 <- data.frame(Funktionsart)
-Decisionnumber225 <- nrow(sqldf(" SELECT * FROM Entscheidungshilfe225 WHERE Funktionsart IN('Operandenfunktion','Funktionsteile') "))
-Program225 <- c('~/Tabellenkalkultionsumwandler/Einzelzellwertausgabe.R','~/Tabellenkalkultionsumwandler/Schleifenzellartausgabe.R')
-Numberoptions225 <- c(0, 1)
-Decisionprogram225 <- data.frame(Program225, Numberoptions225)
-Programres225 <- paste0('SELECT Program225 FROM Decisionprogram225 WHERE Numberoptions225 =',Decisionnumber225)
-Programoutput225 <- sqldf(Programres225)
-Programoutputres225 <- Programoutput225$Program225
-source(Programoutputres225, encoding = 'UTF-8')
