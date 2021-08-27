fbf23a3589540d58d46b8dd6b532f7742ecce95d
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 19:09:13 +0200

    Delete Drittebenverarbeitung.R

--- Drittebenverarbeitung.R
+++
@@ -1,4 +0,0 @@
-Funktionsstartliste <- data.frame(Formel)
-Funktionsstartliste$Nummer <- 1
-Funktionsstartliste$Nummer <- cumsum(Funktionsstartliste$Nummer)
-source('~/Tabellenkalkultionsumwandler/Argumentlistenschleife.R')
