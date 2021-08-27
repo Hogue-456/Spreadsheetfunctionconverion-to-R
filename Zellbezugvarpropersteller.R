770d326c415c2548f3e5e84fc193e37126feb4bd
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 19:12:18 +0200

    Delete Zellbezugvarpropersteller.R

--- Zellbezugvarpropersteller.R
+++
@@ -1,2 +0,0 @@
-Zellbezuege <- sqldf(" SELECT COUNT(DISTINCT Res) AS Zellbezug, Res FROM Argumentliste WHERE Funktionsart IN('Zellbezug') GROUP BY Res")
-Zellbezuege$Varprop <- paste0('Gesamtliste$Var',cumsum(Zellbezuege$Zellbezug))
