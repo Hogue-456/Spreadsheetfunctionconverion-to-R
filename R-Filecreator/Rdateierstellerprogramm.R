f49c057e6fb0daa616cec5f77d8fe47349b742ea
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 20:16:17 +0200

    Add files via upload

---
+++ R-Filecreator/Rdateierstellerprogramm.R
@@ -0,0 +1,4 @@
+write.table(Proglist28, file = paste0('C:\\Users\\timh9\\Documents\\Tabellenkalkulationumwandler\\Programmausführung\\Testprogramm20210728\\',Programmteil,'20210728.R'), append = FALSE, quote = FALSE, sep = " ",
+            eol = "\n", na = "NA", dec = ",", row.names = FALSE,
+            col.names = FALSE,
+            fileEncoding = 'UTF-8')
