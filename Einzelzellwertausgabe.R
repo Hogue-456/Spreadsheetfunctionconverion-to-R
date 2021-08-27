97e315154cd6ef48144ea630ab1831f7d3dcc3ab
Hogue-456 <89610189+Hogue-456@users.noreply.github.com>
2021-08-27 19:09:37 +0200

    Delete Einzelzellwertausgabe.R

--- Einzelzellwertausgabe.R
+++
@@ -1,9 +0,0 @@
-Ab <- 0
-Bis <- 0
-Fnct <- Bestandteile$Extrakt
-Res <- 0
-Nummer <- 0
-Programm <- 0
-Programm22 <- 0
-Funktionsart <- Bestandteile$Funktionsart
-Argumentliste <- data.frame(Ab, Bis, Fnct, Res, Nummer, Programm, Programm22, Funktionsart)
